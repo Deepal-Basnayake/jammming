c44551ac56a40e8e83598425bf63426c05778dd4
Deepal-Basnayake <deepal.basnayake@yahoo.com>
2024-06-07 15:23:05 +0100

    adding search bar and heading

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
+      <div className='heading'>
       <h1>Jammming App</h1>
+      </div>
+      <div className='searchbar'>
+        <input></input>
+      </div>
     </div>
   );
 }
